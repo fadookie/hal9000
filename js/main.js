704b5eceebf008b61d07eabd0686a1612215cee9
Eliot <fadookie@gmail.com>
2013-06-02 00:37:56 -0700

    more cleanup and fix logging bug

--- js/main.js
+++ js/main.js
@@ -3,16 +3,14 @@ meSpeak.loadVoice("voices/en/en.json");
 
 $( "#speechToTextBox" ).on('input', function() {
     console.log('TextChange');
-//    searchControl.execute($( "#speechToTextBox" ).val());
     var queryURL = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + $.param({q: $( "#speechToTextBox" ).val()}) + '&callback=?';
     console.log(queryURL);
     jQuery.getJSON(queryURL, function(data) {
-//      var items = [];
         var content = data.responseData.results[0].content; 
         if(content)
         {
             var contentStrip = $('<p>'+content+'</p>').text();
-            console.log(data.responseData.results[0]. urlcontentStrip);
+            console.log(data.responseData.results[0].url + " " + contentStrip);
             meSpeak.speak(contentStrip);
         } else {
             console.log("no content found.");
